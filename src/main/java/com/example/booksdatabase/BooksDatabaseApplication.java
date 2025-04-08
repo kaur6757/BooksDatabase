9825416be37cbcfca7716aeabd23b30a4fe872dd
Navpreet Kaur <kaur6757@sheridancollege.ca>
2025-04-07 22:32:56 -0400

    Initial commit: Server side

---
+++ src/main/java/com/example/booksdatabase/BooksDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksDatabaseApplication.class, args);
+	}
+
+}
