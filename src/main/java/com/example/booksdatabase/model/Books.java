4b6580ed2c8480007ad226c341c3be559a7b5a87
Navpreet Kaur <kaur6757@sheridancollege.ca>
2025-04-08 12:25:20 -0400

    Implemented Books class withproperties(Id,title,authorName,price)

---
+++ src/main/java/com/example/booksdatabase/model/Books.java
@@ -0,0 +1,55 @@
+package com.example.booksdatabase.model;
+
+public class Books {
+    //attributes
+    private Long id;
+    private String title;
+    private String authorName;
+    private double price;
+
+    //default constructor
+    public Books() {
+    }
+
+    //Constructor
+    public Books(Long id, String title, String authorName, double price) {
+        this.id = id;
+        this.title = title;
+        this.authorName = authorName;
+        this.price = price;
+    }
+
+    //Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/example/booksdatabase/model/Review.java
@@ -0,0 +1,7 @@
+package com.example.booksdatabase.model;
+
+public class Review {
+
+
+
+}
