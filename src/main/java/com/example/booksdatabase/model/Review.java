7ddb8e5ec3679e463b31e896e263b07761a6fdd7
Navpreet Kaur <kaur6757@sheridancollege.ca>
2025-04-08 12:27:44 -0400

    Implemented Review class with properties(Id,bookId,reviewerName,ratting(outof5),comment)

--- src/main/java/com/example/booksdatabase/model/Review.java
+++ src/main/java/com/example/booksdatabase/model/Review.java
@@ -2,6 +2,64 @@ package com.example.booksdatabase.model;
 
 public class Review {
 
+    //Properties
+    private Long id;
+    private Long bookId;
+    private String reviewerName;
+    private int rating;
+    private String comment;
+
+    //Default constructor
+    public Review() {
+    }
+    public Review(Long id, Long bookId, String reviewerName, int rating, String comment) {
+        this.id = id;
+        this.bookId = bookId;
+        this.reviewerName = reviewerName;
+        this.rating = rating;
+        this.comment = comment;
+
+    }
+    //Getters and Setters
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getReviewerName() {
+        return reviewerName;
+    }
+
+    public void setReviewerName(String reviewerName) {
+        this.reviewerName = reviewerName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
